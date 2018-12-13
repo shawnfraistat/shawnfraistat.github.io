4ae25a6dd12cefac84a9adee411ecfe481d7ed4a
shawnfraistat <shawn.fraistat@gmail.com>
2018-12-13 16:30:49 -0500

    Add Animation on About and Add Sass
    Add an animation so that the about section slides in and out from the
    left on scroll. Also added sass and refactored css.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,22 @@
 'use strict'
 
+const scrollEffects = function () {
+  const scrollPercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+  if (scrollPercent > 0.05) {
+    console.log('moved past 0.1')
+    $('.about-div').css('opacity', 1)
+    $('.about-div').css('transform', 'translateX(300px)')
+  }
+  if (scrollPercent <= 0.05) {
+    console.log('moved past 0.1')
+    $('.about-div').css('opacity', 0)
+    $('.about-div').css('transform', 'translateX(-300px)')
+  }
+}
+
 $(() => {
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       let target = $(this.hash)
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
       if (target.length) {
@@ -28,4 +42,8 @@ $(() => {
   $(function () {
     $('.resume-link').EZView()
   })
+
+  window.addEventListener('scroll', scrollEffects)
 })
+
+// create scroll effects
